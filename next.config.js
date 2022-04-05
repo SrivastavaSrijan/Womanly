4a8e51cfb8c60d041368fecd72e23ac536c38845
Srijan Srivastava <srijan.srivastava35@gmail.com>
2022-04-05 19:41:51 +0530

    🌍fixed vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    DEV_URL: 'http://localhost:3000',
+    PROD_URL: 'https://womanly.srijansrivastava.tech'
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: `${
+    process.env.NODE_ENV === 'production'
+      ? process.env.PROD_URL
+      : process.env.DEV_URL
+  }/api/graphql`,
   cache: new InMemoryCache()
 });
 
